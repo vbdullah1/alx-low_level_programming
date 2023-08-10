4b5014bb572a56624c09fe86fc7e0534b70d3ff3
abdullah <v11@example.com>
2023-08-10 07:58:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-int main (void)
+/**
+*Hello C
+*/
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
